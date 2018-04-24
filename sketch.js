252da6a691fc0aaa661ec77568dcc1ae2005dfc8
samostanek <samostanek@gmail.com>
2018-04-24 11:48:47 +0200

    Add gridInit(space)
    Adde gridInit for node space initialization.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+var grid;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(400, 400);
+
+	grid = gridInit(20);
 }
 
 function draw() {
 
+}
+
+
+function gridInit(space) {
+	// Get width and height of grid based on size of cell
+	let cols = width/space;
+	let rows = height/space;
+
+	// Make x part of grid
+	let out = new Array(cols);
+	// Add cell for each row in col i
+	for (col in out) col = new Array(rows);
+
+	return out
 }
