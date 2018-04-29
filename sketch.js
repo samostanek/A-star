4997ea6849dd35103e7c74964c9b83986dfb543c
samostanek <samostanek@gmail.com>
2018-04-29 17:41:57 +0200

    Added drawing grid

--- sketch.js
+++ sketch.js
@@ -1,31 +1,47 @@
-var grid;
+var grid, start, end, spc;
+var openSet, closedSet;
 
 function setup() {
-	createCanvas(400, 400);
+  createCanvas(400, 400);
 
-	grid = gridInit(50);
+  spc = 50;
+  grid = gridInit(spc);
 
-	for (var i = 0; i < grid.length; i++) {
-		for (var j = 0; j < grid[i].length; j++) {
-			grid[i][j] = new Spot();
-		}
-	}
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+      grid[i][j] = new Spot(i, j);
+    }
+  }
+
+  start = grid[0][0];
+  end = grid[grid.length - 1][grid[0].length - 1];
+
+  openSet = [start];
+  closedSet = [];
 }
 
 function draw() {
+  if (openSet.length > 0) {
+  } else {
+    console.log("DONE!");
+    noLoop();
+  }
 
+  // Draw grid
+  for (let i of grid) for (let j of i) j.show(-1, spc - 1);
+  for (let i of openSet) i.show(color(0, 255, 0), spc - 1);
+  for (let i of closedSet) i.show(color(255, 0, 0), spc - 1);
 }
 
-
 function gridInit(space) {
-	// Get width and height of grid based on size of cell
-	let cols = width/space;
-	let rows = height/space;
+  // Get width and height of grid based on size of cell
+  let cols = width / space;
+  let rows = height / space;
 
-	// Make x part of grid
-	let out = new Array(cols);
-	// Add cell for each row in col i
-	for (var i = 0; i < out.length; i++) out[i] = new Array(rows);
+  // Make x part of grid
+  let out = new Array(cols);
+  // Add cell for each row in col i
+  for (var i = 0; i < out.length; i++) out[i] = new Array(rows);
 
-	return out
+  return out;
 }
--- spot.js
+++ spot.js
@@ -1,7 +1,19 @@
 class Spot {
-  constructor() {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+
     this.f = 0;
     this.g = 0;
     this.h = 0;
+  }
+
+  show(c, s) {
+    // Argument is width and height of one Spot in grid
+    stroke(0);
+    if (c != -1) fill(c);
+    else noFill();
+    strokeWeight(2);
+    rect(this.x * s, this.y * s, s, s);
   }
 }
